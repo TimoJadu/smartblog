4d913be698ef737d05bc0e27b3f50ff0b57acca8
TimoJadu <subhajit.ece.ind@gmail.com>
2018-03-19 17:51:52 +0000

    path modified

--- smartblogproject/settings.py
+++ smartblogproject/settings.py
@@ -11,9 +11,9 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
-
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
