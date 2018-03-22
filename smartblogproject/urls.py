69cf58315d3205c6f2a84299c3df9edca84fc133
TimoJadu <subhajit.ece.ind@gmail.com>
2018-03-22 14:26:33 +0000

    branch set for thesis work

--- smartblogproject/urls.py
+++ smartblogproject/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls, name="admin"),
     url(r'^posts/', include("smartblogapp.url", namespace="posts")),
     url(r'^$', include("smartblogapp.url", namespace="posts")),
-    # url(r'^$',index),
+    # url(r'^$',index), #PT branch created
 ]
 
 
